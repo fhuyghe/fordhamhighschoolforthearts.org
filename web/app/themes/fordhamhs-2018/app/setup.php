363b072004aaa32595980d1995910d51c69b17d0
Francois Huyghe <francois@13milliseconds.com>
2018-09-27 20:04:23 -0500

    including main.css

--- web/app/themes/fordhamhs-2018/app/setup.php
+++ web/app/themes/fordhamhs-2018/app/setup.php
@@ -11,7 +11,7 @@ use Roots\Sage\Template\BladeProvider;
  * Theme assets
  */
 add_action('wp_enqueue_scripts', function () {
-    //wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
+    wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
     wp_enqueue_style('fontAwesome', 'https://pro.fontawesome.com/releases/v5.3.1/css/all.css', null, true);
 }, 100);
