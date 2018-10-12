026ee90788a66b2043f64618103d87de83dd3072
Francois Huyghe <francois@13milliseconds.com>
2018-10-11 20:54:09 -0500

    Finally using the right font

--- web/app/themes/fordhamhs-2018/resources/views/search.blade.php
+++ web/app/themes/fordhamhs-2018/resources/views/search.blade.php
@@ -3,16 +3,27 @@
 @section('content')
   @include('partials.page-header')
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
-  @endif
+  <div class="columns">
+      <div class="column is-8">
+        <div class="search-content">
+          <h1>Search Results</h1>
+      @if (!have_posts())
+        <div class="alert alert-warning">
+          {{ __('Sorry, no results were found.', 'sage') }}
+        </div>
+        {!! get_search_form(false) !!}
+      @endif
 
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.content-search')
-  @endwhile
+      @while(have_posts()) @php the_post() @endphp
+        @include('partials.content-search')
+      @endwhile
+    </div>
+  </div>
+
+  <div class="sidebar column is-4">
+    @include('partials.sidebar')
+  </div>
+</div>
 
   {!! get_the_posts_navigation() !!}
 @endsection
