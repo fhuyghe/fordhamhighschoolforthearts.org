f49b4b03c4dd8a9beeb3655934aa7a026bb8b197
Francois Huyghe <francois@13milliseconds.com>
2018-10-29 16:10:58 -0500

    Added favicons

--- web/app/themes/fordhamhs-2018/resources/views/partials/head.blade.php
+++ web/app/themes/fordhamhs-2018/resources/views/partials/head.blade.php
@@ -2,5 +2,16 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="apple-touch-icon" sizes="180x180" href="@asset('images/favicon/apple-touch-icon.png')">
+  <link rel="icon" type="image/png" sizes="32x32" href="@asset('images/favicon/favicon-32x32.png')">
+  <link rel="icon" type="image/png" sizes="16x16" href="@asset('images/favicon/favicon-16x16.png')">
+  <link rel="manifest" href="@asset('images/favicon/site.webmanifest')">
+  <link rel="mask-icon" href="@asset('images/favicon/safari-pinned-tab.svg')" color="#5bbad5">
+  <link rel="shortcut icon" href="@asset('images/favicon/favicon.ico')">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="msapplication-config" content="@asset('images/favicon/browserconfig.xml')">
+  <meta name="theme-color" content="#ffffff">
+
   @php wp_head() @endphp
 </head>
