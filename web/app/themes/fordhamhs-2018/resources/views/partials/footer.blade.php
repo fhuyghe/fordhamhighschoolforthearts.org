7cacfb72b35dd750c15b0dc4bc82b2e41f6fe337
Francois Huyghe <francois@13milliseconds.com>
2018-11-14 15:00:55 -0600

    Last footer changes

--- web/app/themes/fordhamhs-2018/resources/views/partials/footer.blade.php
+++ web/app/themes/fordhamhs-2018/resources/views/partials/footer.blade.php
@@ -3,20 +3,21 @@
     @php dynamic_sidebar('sidebar-footer') @endphp
     <div class="columns is-6">
     <div class="column is-3">
-      <b><?php echo apply_filters( 'the_content', carbon_get_theme_option( 'crb_address' ) ); ?></b>
-      <p>
-        <b>Phone:</b> <?php echo carbon_get_theme_option( 'crb_phone' ); ?><br>
-        <b>Fax:</b> <?php echo carbon_get_theme_option( 'crb_fax' ); ?>
-      </p>
+      <h6>Fordham High School For The Arts</h6>
     </div>
     <div class="column is-3">
-      <?php echo apply_filters( 'the_content', carbon_get_theme_option( 'crb_footer_col2' ) ); ?>
+        <b><?php echo apply_filters( 'the_content', carbon_get_theme_option( 'crb_address' ) ); ?></b>
+        <p>
+          <b>Phone:</b> <?php echo carbon_get_theme_option( 'crb_phone' ); ?><br>
+          <b>Fax:</b> <?php echo carbon_get_theme_option( 'crb_fax' ); ?>
+        </p>
   </div>
-  <div class="column is-2">
-    <a class="button" href="/contact">Contact Us</a> 
+  <div class="column is-3">
+      <?php echo apply_filters( 'the_content', carbon_get_theme_option( 'crb_footer_col2' ) ); ?>
 </div>
-<div class="column is-4 clearfix"> 
-    @php echo do_shortcode('[wpgmza id="1"]') @endphp
+<div class="column is-3 clearfix"> 
+    <a class="button" href="/contact">Contact Us</a> 
+    @php /*echo do_shortcode('[wpgmza id="1"]')*/ @endphp
 </div>
   </div>
     <div class="copyrights"><?php echo carbon_get_theme_option( 'crb_footer_copyright' ); ?></div>
